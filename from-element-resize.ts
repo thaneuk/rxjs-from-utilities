af3d0ac2ef0051272d65aa57d5d8f06462bba31c
Gregory Jackson <gregory@fates.co.uk>
2024-07-08 10:36:42 -0400

    Update from-element-resize.ts

--- from-element-resize.ts
+++ from-element-resize.ts
@@ -1,7 +1,7 @@
 import { Observable, Subscriber } from 'rxjs';
 
 /** will always emit initially, suggest skip(1) if not desired */
-export const fromElementResize = (element: HTMLElement) => {
+export const fromElementResize = (element: Element) => {
   return new Observable((subscriber: Subscriber<ResizeObserverEntry[]>) => {
     let resizeObserver: ResizeObserver | null = new ResizeObserver((entries: ResizeObserverEntry[]) =>
       subscriber.next(entries)
@@ -10,7 +10,7 @@ export const fromElementResize = (element: HTMLElement) => {
     resizeObserver?.observe(element);
 
     return (): void => {
-      resizeObserver?.unobserve(element);
+      resizeObserver?.disconnect(element);
       resizeObserver = null;
     };
   });
