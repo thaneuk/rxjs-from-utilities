83f9983911f1bb5de37d833d33f2586c2129037d
Gregory Jackson <gregory@fates.co.uk>
2024-07-08 10:39:26 -0400

    Update from-dom-mutation.ts

--- from-dom-mutation.ts
+++ from-dom-mutation.ts
@@ -1,6 +1,6 @@
 import { Observable, Subscriber } from 'rxjs';
 
-export const fromDomMutation = (element: HTMLElement, options?: MutationObserverInit) => {
+export const fromDomMutation = (element: Element, options?: MutationObserverInit) => {
   return new Observable((subscriber: Subscriber<MutationRecord[]>) => {
     let mutationObserver: MutationObserver | null = new MutationObserver((mutations: MutationRecord[]) =>
       subscriber.next(mutations)
